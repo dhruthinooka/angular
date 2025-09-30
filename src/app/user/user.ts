a869ee6045e5e3750b0b3c6652410c2c3068842e
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-09-30 14:13:34 +0200

    Signal Inputs

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,12 +10,20 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css'
 })
 export class User {
-  @Input({ required: true }) avatar!: string;
-  @Input() name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
+
+  onSelectUser() {
+    this.avatar.set();
   }
-
-  onSelectUser() {}
 }
