c90a9a0a75bfb81c37fe59093dbe931b572e8b15
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-09-30 11:44:06 +0200

    Component Inputs, Required & Optional Inputs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header";
 import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { User } from "./user/user";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  users = DUMMY_USERS;
+}
