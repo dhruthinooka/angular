88bed4dfacda4631d72c0eebb9e1f209d060bca8
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-06 15:41:20 +0200

    Deleting the Task functionality

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { type TaskInterface } from './task.model';
 
 @Component({
@@ -9,4 +9,8 @@ import { type TaskInterface } from './task.model';
 })
 export class Task {
   @Input({ required: true }) task!: TaskInterface;
+  @Output() complete = new EventEmitter<string>();
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
