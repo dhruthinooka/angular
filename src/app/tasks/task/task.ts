44ebe7bbeb350eef39d79694492a1c3af9392772
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-06 10:44:20 +0200

    Outputting Task data in Task Component

---
+++ src/app/tasks/task/task.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+
+interface TaskInterface {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.css'
+})
+export class Task {
+  @Input({ required: true }) task!: TaskInterface;
+}
